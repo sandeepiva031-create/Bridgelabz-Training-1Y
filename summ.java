3b8cb4349e6209a494e0534d2a27d62bffa0e1bd
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-08 14:28:01 +0530

    third commit

---
+++ summ.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static void main(String[] args) {
+        // Declare and initialize two integer variables
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Add the numbers and store the result in a third variable
+        int sum = num1 + num2;
+        
+        // Print the sum to the console
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}

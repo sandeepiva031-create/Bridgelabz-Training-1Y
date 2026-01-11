a05d5226819a7595289395a18c22ae7be0412d34
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-11 12:39:08 +0530

    VolumeofCylinder

---
+++ VolumeOfCylinder.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter radius of cylinder:");
+        double radius = sc.nextDouble();
+
+        
+        System.out.println("Enter height of cylinder:");
+        double height = sc.nextDouble();
+
+        
+        double volume = Math.PI * radius * radius * height;
+
+        
+        System.out.printf("Volume of Cylinder is: %.2f%n", volume);
+
+        sc.close();
+    }
+}

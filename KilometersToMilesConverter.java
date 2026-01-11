52d348f6d7f42d99f8f0e44be6aa6e697fccbe19
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-11 18:21:53 +0530

    KilometersToMilesConverter

---
+++ KilometersToMilesConverter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KilometersToMilesConverter {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = scanner.nextDouble();
+
+        double miles = kilometers * 0.621371;
+
+        System.out.printf("%.2f kilometers is equal to %.2f miles.%n", kilometers, miles);
+        
+        scanner.close();
+    }
+}

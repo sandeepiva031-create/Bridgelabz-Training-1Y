aecb60f7de7cb12e6fe4e54c86ec0169a02cab6b
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-11 12:27:17 +0530

    SumOFNum

---
+++ SumOfTwoNum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SumOfTwoNum{
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter First Number :");
+        int num1= sc.nextInt();
+
+        System.out.println("Enter Second Number :");    
+        int num2 =sc.nextInt();
+
+        int sum = num1 + num2; 
+
+        System.out.println("Sum of Two Number is :" + sum);
+       sc.close();                     
+    }
+}

a9f4840a430bd8db9a4531257f2395291b13f96d
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-15 23:31:54 +0530

    Marks

---
+++ Basic Programmes/Marks.java
@@ -0,0 +1,9 @@
+public class Marks {
+    public static void main(String[] args){
+        int maths=94;
+        int physics=95;
+        int chemistry=96;
+        double avg=(94+95+96)/3.0;
+        System.out.println("Sam's average marks in PCM is " +avg);
+    }
+}

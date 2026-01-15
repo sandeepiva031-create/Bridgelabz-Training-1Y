565b5b233c2f10a00f418503342b3f26dfe9c881
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-15 23:20:57 +0530

    DistanceConversion.java

---
+++ Basic Programmes/DistanceConversion.java
@@ -0,0 +1,7 @@
+public class DistanceConversion {
+    public static void main(String[] args){
+        double dis=10.8;
+        double result=10.8*0.6;
+        System.out.println("The distance "+ dis + " in miles is "+result);
+    }
+}

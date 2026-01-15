90cc79eac8bf679ed11590bcdaed77091c9ef852
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-15 23:36:04 +0530

    Pens Distribution

---
+++ Basic Programmes/PensDistribution.java
@@ -0,0 +1,10 @@
+
+public class PensDistribution {
+    public static void main(String[] args) {
+        int pens = 14;
+        int students = 3;
+
+        System.out.println("The Pen Per Student is " + (pens / students) +
+                        " and the remaining pen not distributed is " + (pens % students));
+    }
+}

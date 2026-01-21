bbe95d8f045fd5919c2d23e5b84a1fd3d5b3a1f0
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-22 00:04:58 +0530

    GreatestFactorWhile

---
+++ Basic Programmes/GreatestFactor.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GreatestFactor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a positive integer:");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            int greatestFactor = 1;
+            for (int i = number - 1; i >= 1; i--) {
+                if (number % i == 0) {
+                    greatestFactor = i;
+                    break;
+                }
+            }
+            System.out.println("Greatest factor of " + number + " besides itself is: " + greatestFactor);
+        }
+
+        sc.close();
+    }
+}

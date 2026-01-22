f9c6d328d4b4867e6ac277d7447d0c19e0b9ad98
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-23 02:16:29 +0530

    Increment.java

---
+++ Basic Programmes/Increment.java
@@ -0,0 +1,25 @@
+public class Increment {
+    public static void main(String[] args) {
+        int i=0;
+        System.out.println(i++); //0
+        i++;
+        System.out.println(i++);  //2
+        System.out.println(i);    //3
+        System.out.println(i++);  //3
+        
+        for(int k=0; k<5; k++) {
+            System.out.println("for loop  "+k);}
+
+        int j=10;
+        while(j>0) {
+            System.out.println("while loop  "+j);
+            j--;
+        
+        }
+    }
+}
+
+            
+                
+
+

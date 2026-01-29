5585797a40805d4def20cab59cbfc1778efd0599
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-01-30 03:01:51 +0530

    Fibbonacci Series

---
+++ Basic Programmes/Fibbonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fibbonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms in the Fibonacci sequence: ");
+        int n = scanner.nextInt();
+        
+        int a = 0, b = 1;
+        System.out.println("Fibonacci Sequence:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        scanner.close();
+    }
+}

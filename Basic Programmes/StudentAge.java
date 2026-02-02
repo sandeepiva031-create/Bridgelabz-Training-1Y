36d4b9317a057f61e71d7407046f058369f3b632
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-02-02 19:12:31 +0530

    Keep only README.md in Arrays branch

---
+++ Basic Programmes/StudentAge.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StudentAge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+      
+        int[] ages = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ages[i] = sc.nextInt();
+        }
+
+    }   
+    
+    }

9bca68eb2f0e5590dbe9c57e384b252438c19bad
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-02-10 23:58:02 +0530

    String Index Out Of Bounds

--- src/com/gla/string/StringIndexOutOfBoundsDemo.java
+++ src/com/gla/string/StringIndexOutOfBoundsDemo.java
@@ -1,4 +1,32 @@
 package com.gla.string;
 
+import java.util.Scanner;
+
 public class StringIndexOutOfBoundsDemo {
+
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length()));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException caught");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+
+        try {
+            generateException(text);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Exception occurred in generateException()");
+        }
+
+        handleException(text);
+        sc.close();
+    }
 }

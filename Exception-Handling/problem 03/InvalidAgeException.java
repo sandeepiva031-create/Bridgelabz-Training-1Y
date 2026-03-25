6a614e63df1b68139ba32ac604063a22a173c0f0
sandeepkumar0113 <sandeep.iva031@gmail.com>
2026-03-25 22:55:51 +0530

    Added exception handling problems

---
+++ Exception-Handling/problem 03/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package com.gla.exception-handling.problem3;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
